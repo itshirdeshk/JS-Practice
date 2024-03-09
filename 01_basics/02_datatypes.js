ac859df0d183b0ce8dfe33ccc0cc7eb128529504
Hirdesh Khandelwal <61307244+itshirdeshk@users.noreply.github.com>
2024-03-09 09:15:06 +0000

    Datatypes Practice

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// It will not work because we are using nodejs, not browser
+// alert(3 + 3)
+
+console.log(3 + 3);
+console.log("Hirdesh");
+
+// Datatypes
+// 1. Number.
+// 2. Bigint.
+// 3. String.
+// 4. Boolean.
+// 5. Null => Standlone value
+// 6. Undefined.
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
