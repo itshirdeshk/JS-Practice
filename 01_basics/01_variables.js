9bdf7b0af95a40816d036d2a34789a5d00a432a1
Hirdesh Khandelwal <61307244+itshirdeshk@users.noreply.github.com>
2024-03-09 09:00:15 +0000

    Variables Practice

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "hirdesh@google.com"
+var accountPassword = "jio0phone"
+accountCity = "Agra"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
