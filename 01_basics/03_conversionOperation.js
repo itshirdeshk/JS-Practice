f8e20cbcb172689c1ac02af0c427af539191773a
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-03-09 17:14:09 +0530

    Operation Practice

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueTONumber = Number(score);
-console.log(valueTONumber);
-console.log(typeof valueTONumber);
+// console.log(valueTONumber);
+// console.log(typeof valueTONumber);
 
 // "33" => 33
 // "33abc" => NaN (Not a Number)
@@ -14,7 +14,33 @@ console.log(typeof valueTONumber);
 let isLoggedIn = 1
 
 let boolIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolIsLoggedIn);
+// console.log(boolIsLoggedIn);
 
 // 1 => true; 0 => false;
 // "hirdesh" => true; "" => false
+
+// ************** Opeartions ************* //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " hirdesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "1");
+
+console.log(+true);
+console.log(+"");
